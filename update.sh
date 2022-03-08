fb24b8e354dd1cb51bb9a1996ea4f4a8e6f78a24
Niklas Hansen <nikchha@gmail.com>
2022-03-08 18:17:17 +0100

    update script

---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+git fetch -vp --tags &&
+    git rebase origin/"$(git rev-parse --abbrev-ref HEAD)" &&
+    git submodule update --init --recursive &&
+    ./install.sh &&
+    echo "Update succeeded" ||
+    echo "Update failed, check output"
